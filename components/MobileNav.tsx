b5a748f309a505139a0df0faa583f784462924cb
Rodrigo Santos <rodrigorafaelsantos7@icloud.com>
2024-07-23 11:12:26 +0100

    Mobile Nav done

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { CiMenuFries } from "react-icons/ci";
@@ -37,7 +42,33 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         {/* logo */}
-        <div>logo</div>
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href={"/"}>
+            <h1 className="text-4xl font-semibold">
+              Rodrigo <span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* Nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <SheetClose asChild key={index}>
+                <Link
+                  href={link.path}
+                  key={index}
+                  className={`${
+                    link.path === pathname &&
+                    " text-accent border-b-2 border-accent"
+                  }
+                  text-xl capitalize hover:text-accent transition-all`}
+                >
+                  {link.name}
+                </Link>
+              </SheetClose>
+            );
+          })}
+        </nav>
       </SheetContent>
     </Sheet>
   );
