851e00c803390d3a11ed28eb1d6f7814ebe5f526
Rodrigo Santos <rodrigorafaelsantos7@icloud.com>
2024-07-23 11:54:11 +0100

    Homepage added the title TODO: Style and photo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+const Home = () => {
   return (
-    <h1>Homepage</h1>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col lg:flex-row items-center justify-between lg:pt-8 lg:pb-24">
+          {/* Text */}
+          <div className="text-center lg:text-left">
+            <span>Software Developer</span>
+            <h1>
+              Hello I'm <br /> <span>Rodrigo Santos</span>{" "}
+            </h1>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
